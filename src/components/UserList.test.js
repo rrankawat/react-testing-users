814013b24d3326c0fe27c6cc7d2e8ec2e86da794
Rakesh Rankawat <rakesh.rankawat@outlook.com>
2023-01-20 00:11:57 +0530

    Avoiding repeatation

--- src/components/UserList.test.js
+++ src/components/UserList.test.js
@@ -1,14 +1,18 @@
 import { render, screen, within } from '@testing-library/react'
 import UserList from './UserList'
 
-test('render one row per user', () => {
+function renderComponent() {
   const users = [
     { name: 'Jane', email: 'jane@gmail.com' },
     { name: 'Sam', email: 'sam@gmail.com' },
   ]
-
-  // Render the component
   render(<UserList users={users} />)
+
+  return { users }
+}
+
+test('render one row per user', () => {
+  renderComponent()
 
   // Find all rows in the table
   // screen.logTestingPlaygroundURL()
@@ -19,12 +23,7 @@ test('render one row per user', () => {
 })
 
 test('render the email and name of each other', () => {
-  const users = [
-    { name: 'Jane', email: 'jane@gmail.com' },
-    { name: 'Sam', email: 'sam@gmail.com' },
-  ]
-
-  render(<UserList users={users} />)
+  const { users } = renderComponent()
 
   for (let user of users) {
     const name = screen.getByRole('cell', { name: user.name })
